038d3d16024b07a5499116b27151d3dce4497c34
ubaydatimomo-lab <ubaydatimomo@gmail.com>
2026-09-07 17:51:34 +0100

    installed @actions/core@^1.10.1 as the other version is older

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
-    console.log( `Hello, ${nameToGreet}!` );
+    console.log(`Hello, ${nameToGreet}!`);
     core.setOutput('time', new Date().toTimeString());
 } catch (error) {
     core.setFailed(error.message);
