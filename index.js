92bac8379a7a00c862f9595b8370e720a282a64d
ubaydatimomo-lab <ubaydatimomo@gmail.com>
2026-09-07 17:28:54 +0100

    fixed @actions/core import path and rebuild dist

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const core = require('actions/core')
+const core = require('@actions/core');
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello', ${nameToGreet}!`);
+    console.log( `Hello, ${nameToGreet}!` );
     core.setOutput('time', new Date().toTimeString());
 } catch (error) {
     core.setFailed(error.message);
